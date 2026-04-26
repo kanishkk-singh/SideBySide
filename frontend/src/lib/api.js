0ac4ede4e0bd5abca0e0949204f80d898431f5fa
kanishkk-singh <kanishkkumarsingh337@gmail.com>
2026-04-27 00:11:14 +0530

    final api fix

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://your-backend-url.onrender.com/api'
+  baseURL: 'https://sidebyside-mf6y.onrender.com/api'
 });
 
 api.interceptors.request.use((config) => {
